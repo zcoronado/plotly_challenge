f90e524b678c82e71e25015ce8424560dd743bed
Zaira Coronado <coronado.zaira@gmail.com>
2021-08-08 13:42:44 -0700

    plots

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,40 +1,107 @@
-d3.json('samples.json').then(({names})=>{
-    names.forEach(name => {
-        d3.select('select').append('option').text(name)
-    });
+function getPlot(id) {
+//Read json file
+d3.json("data/samples.json").then((data) => {   
+    console.log(data) 
 
-    renderData();
-});
+//Filter Data for top 10 
+var samples = data.samples.filter(top10 => top10.id.toString() === id)[0]; 
+
+//Get top 10 
+var samplevalues = (sample.sample_values.slice(0,10)).reverse(); 
+
+//Top 10 OTU
+var OTU_top = (samples.otu_ids.slice(0,10)).reverse(); 
+
+//Get OTU ID
+var OTU_id = OTU_top.map( x => "OTU" + x); 
+
+// console.log(OTU_id); 
+
+//Get labels for plot 
+var labels = samples.otu_labels.slice(0,10); 
+
+//Create trace 
+var trace = {
+    x: samplevalues,
+    y: OTU_id,
+    text: labels,  
+    type: "bar", 
+    orientation: "h", 
+}; 
+
+//Create data variable 
+var data = [trace];
+
+//create layout variable to set plot layout 
+var layout = {
+    title: "Top 10 OTU", 
+    yaxis: { 
+        tickmode: "linear",
+    }, 
+    margin: {
+        l:100,
+        r:100,
+        t:100,
+        b:30
+    }
+}; 
+
+// Create the bar plot 
+Plotly.newPlot("bar", data, layout); 
+
+//Create bubble chart 
+var trace1 = { 
+    x: samples.otu_ids, 
+    y: samples.sample_values, 
+    mode: "markers", 
+    marker: {
+        size:samples.sample_values, 
+        color: samples.otu_ids
+    }, 
+    text: samples.otu_labels
+}; 
+
+//Sett Bubble Bar layout 
+var layout_bar = {
+    xaxis:{title: "OTU ID"}, 
+    height:600, 
+    width: 1000
+}; 
+
+
+//Create data variable 
+var data1 = [trace1]; 
+
+//Create bubble plot 
+Plotly.newPlot("bubble", data1, layout_b); 
+
+}); 
+}
+
+
+
+
+
+
+
+//change functio
+function optionChanged(id) {
+    renderData(id);
+    getPlot0(id); 
+ }
 
 function renderData() {
-    var pick = d3.select('select').property('value');
+    var pick = d3.select("#selDataset");
 
-    d3.json('samples.json').then(({metadata, samples}) => {
-        var meta = metadata.filter( obj => obj.id == pick)[0]; 
-        var sample = samples.filter( obj => obj.id == pick)[0]; 
-        console.log(sample); 
+//Read
+
+    d3.json("data/samples.json").then((data) => { 
+        console.log(data); 
 
         d3.select('.panel-body').html('');
         Object.entries(meta).forEach(([key,val]) =>{
            d3.select('.panel-body').append('h5').text(key.toUpperCase()+': '+ val)
-        })
+        })}; 
 
 
 
-
-        var data = [
-            {
-              x: ['giraffes', 'orangutans', 'monkeys'],
-              y: [20, 14, 23],
-              type: 'bar',
-              orientation: 'h'
-            }
-          ];
-          
-          Plotly.newPlot('bar', data);
-    })
-};
-
-function optionChanged() {
-    renderData();
-};
